502a65fb0d086355e5c7653b2d38fb82679f7963
kosupe <kosupppppeeeee@gmail.com>
2023-10-30 23:42:41 +0900

    flet test

--- class_file/test_class.py
+++ class_file/test_class.py
@@ -1,3 +1,22 @@
-import flet
+import flet as ft
 
-a = "aaa"
+def main(page: ft.Page):
+    #テキスト
+    t = ft.Text(value="Hello")
+    page.add(t)
+    
+    #チェックボックス
+    todo_check = ft.Checkbox(
+        label="ToDo",
+        value=False,
+    )
+    page.add(todo_check)
+    
+    #ボタン
+    def button_clickde(e):
+        page.add(ft.Text("Clicked!"))
+    #                          ボタンのテキスト      押された時に実行されるメソッド
+    page.add(ft.ElevatedButton(text="Click me",      on_click=button_clickde))
+    
+    
+ft.app(target=main)
