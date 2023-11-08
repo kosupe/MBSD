2a02c3d4172921861b3a931c6c27cfb0879e9403
kaoru <satokatsu0214@gmail.com>
2023-11-08 21:39:54 +0900

    array solution version

--- gui_page.py
+++ gui_page.py
@@ -2,50 +2,33 @@ import flet as ft
 import time
 addcounter = 0
 def main(page:ft.Page):
-    page.title = "ごとうぐみ"
-    page.window_width = 650
+    page.title         = "ごとうぐみ"
+    page.window_width  = 650
     page.window_height = 1000
-   
-    target_domain = ft.Ref[ft.TextField]()
-    start_URL = ft.Ref[ft.TextField]()
-    greetings = ft.Ref[ft.Column]()#プリントテスト用
+    target_domains     = [ft.Ref[ft.TextField]()]
+    start_URL          = ft.Ref[ft.TextField]()
+    greetings          = ft.Ref[ft.Column]()#プリントテスト用
     #page.bgcolor = ft.colors.TRANSPARENT
-    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
     
     def button_clicked(e):
         global addcounter
-        target_domains = []
         stack = page.controls.pop()
         stack2 = page.controls.pop()
-        for endpoint,_ in enumerate(range(addcounter+1)):
-            # if target_domain.current.value ==  target_domains:
-            #     print('true')
-            #     page.controls.pop()
-            #     page.update()
-            #     continue
-            """
-            ドメインを格納した配列の中身と一致するドメインが入力された場合に
-            処理をスキップしたいけど完全一致を検証する方法が分からない
-            """
-            target_domains.append(target_domain.current.value)
-            if (endpoint + 1) == (addcounter+1):
-                break
-            page.controls.pop()
-            page.update()
-            time.sleep(1)
-            # if (endpoint+1) < (addcounter+1):
-            #     page.controls.pop()
+        domains = []
+        for target_domain in target_domains:
+            if target_domain.current.value in domains:
+                continue
+            domains.append(target_domain.current.value)
+        
+        print(domains)
 
-        page.add(stack)
         page.add(stack2)
+        page.add(stack)
         greetings.current.controls.append(
-            ft.Text(f"{start_URL.current.value}{target_domains}  ")
+            ft.Text(f"{start_URL.current.value}{domains}")
         )
-        print(target_domains)
-        start_URL.current.value = ""
-        target_domain.current.value = ""
         page.update()
-        # Results = Main(start_URL,target_domain)
+        # Results = Main(start_URL,target_domains)
         pass
 
     def addbutton_clicked(e):
@@ -58,23 +41,27 @@ def main(page:ft.Page):
         """
         stack = page.controls.pop()#ボタン一時退避
         stack2 = page.controls.pop()
-        page.controls.append(ft.TextField(ref=target_domain, label="対象のドメインを入力してください",width=300,height=60))
-        page.add(stack)#ボタン再配置
-        page.add(stack2)
+        target_domains.append(ft.Ref[ft.TextField]())
+        page.controls.append(ft.TextField(ref=target_domains[len(target_domains)-1], label="対象のドメインを入力してください",width=300,height=60))
+        page.add(stack2)#ボタン再配置
+        page.add(stack)
         
         
     def delbutton_clicked(e):
         global addcounter
+        if addcounter <= 0:#ドメイン入力欄を削除する。最低１つは残す
+            return
         stack = page.controls.pop()#ボタン一時退避
-        if addcounter > 0:#ドメイン入力欄を削除する。最低１つは残す
-            page.controls.pop()
-            addcounter -= 1
-        page.add(stack)#ボタン再配置
+        stack2 = page.controls.pop()
+        page.controls.pop()
+        addcounter -= 1
+        page.add(stack2)#ボタン再配置
+        page.add(stack)
         
     page.add(
         ft.TextField(ref=start_URL,label="探索を開始するURLを入力してください",width=550,height=80,autofocus=True,max_length=200),#autofocus=True　初期カーソル
         ft.Column(controls=[
-            ft.TextField(ref=target_domain, label="対象のドメインを入力してください",width=300,height=60),
+            ft.TextField(ref=target_domains[0], label="対象のドメインを入力してください",width=300,height=60),
         ]),
         ft.Row([
             ft.ElevatedButton(
