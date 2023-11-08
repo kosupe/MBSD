a8a3d3ed2e7b2e5f4243ccfbef5cc0c71901e5b5
kaoru <satokatsu0214@gmail.com>
2023-11-08 19:28:32 +0900

    ドメイン複数入力されたとき１つ目のドメインしか取得できません

--- gui_page.py
+++ gui_page.py
@@ -1,4 +1,5 @@
 import flet as ft
+import time
 addcounter = 0
 def main(page:ft.Page):
     page.title = "ごとうぐみ"
@@ -11,23 +12,40 @@ def main(page:ft.Page):
     #page.bgcolor = ft.colors.TRANSPARENT
     # page.vertical_alignment = ft.MainAxisAlignment.CENTER
     
-    
     def button_clicked(e):
+        global addcounter
         target_domains = []
         stack = page.controls.pop()
-        # for _ in page.controls.count:
-        #     target_domains.append(target_domain.current.value)
-        #     page.controls.pop()
+        stack2 = page.controls.pop()
+        for endpoint,_ in enumerate(range(addcounter+1)):
+            # if target_domain.current.value ==  target_domains:
+            #     print('true')
+            #     page.controls.pop()
+            #     page.update()
+            #     continue
+            """
+            ドメインを格納した配列の中身と一致するドメインが入力された場合に
+            処理をスキップしたいけど完全一致を検証する方法が分からない
+            """
+            target_domains.append(target_domain.current.value)
+            if (endpoint + 1) == (addcounter+1):
+                break
+            page.controls.pop()
+            page.update()
+            time.sleep(1)
+            # if (endpoint+1) < (addcounter+1):
+            #     page.controls.pop()
+
         page.add(stack)
-        # target_domains=[domain for domain in target_domain.current.]
+        page.add(stack2)
         greetings.current.controls.append(
-            ft.Text(f"{start_URL.current.value} ")
+            ft.Text(f"{start_URL.current.value}{target_domains}  ")
         )
+        print(target_domains)
         start_URL.current.value = ""
         target_domain.current.value = ""
         page.update()
         # Results = Main(start_URL,target_domain)
-        # del target_domains[:]
         pass
 
     def addbutton_clicked(e):
@@ -39,8 +57,10 @@ def main(page:ft.Page):
         最低限のドメイン入力欄を確保するために０ならばマイナス処理は行わない
         """
         stack = page.controls.pop()#ボタン一時退避
+        stack2 = page.controls.pop()
         page.controls.append(ft.TextField(ref=target_domain, label="対象のドメインを入力してください",width=300,height=60))
         page.add(stack)#ボタン再配置
+        page.add(stack2)
         
         
     def delbutton_clicked(e):
@@ -82,7 +102,7 @@ def main(page:ft.Page):
             ft.ElevatedButton("探索開始", on_click=button_clicked),
         ],alignment=ft.MainAxisAlignment.START,
         ),
-        # ft.Column(ref=greetings),
+        ft.Column(ref=greetings),
     )
     # page.add(ft.FloatingActionButton(icon=ft.icons.ADD, on_click=addbutton_clicked))
 
