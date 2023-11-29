544d2012f29a3750ce0b6e1d6491c24d11267a2d
kaoru <satokatsu0214@gmail.com>
2023-11-29 19:03:02 +0900

    キーワード出力これでできる？

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import flet as ft
-from class_file.Page_class    import Page
-from class_file.Crawler_class import Crawler
+from class_file.Page_class     import Page
+from class_file.Crawler_class  import Crawler
+from class_file.Scraping_class import Scraping
 
-addcounter = 0
 class Top(ft.View):
     def __init__(self):
         self.addcounter = 0
-        start_URL          = ft.Ref[ft.TextField]()
-        target_domains     = [ft.Ref[ft.TextField]()]
+        start_URL      = ft.Ref[ft.TextField]()
+        target_domains = [ft.Ref[ft.TextField]()]
         controls = [
             ft.AppBar(title=ft.Text("Top Page"), bgcolor=ft.colors.SURFACE_VARIANT),
             ft.TextField(ref=start_URL,label="探索を開始するURLを入力してください",on_change=self.changed),
             ft.TextField(ref=target_domains[0], label="対象とするドメインを入力してください",width=300,height=60),
             ft.Row([
-            ft.ElevatedButton(
-            "-",on_click=self.delbutton_clicked),
-            ft.ElevatedButton(
-            "+",on_click=self.addbutton_clicked),
+            ft.ElevatedButton("-",on_click=self.delbutton_clicked),
+            ft.ElevatedButton("+",on_click=self.addbutton_clicked),
             ft.ElevatedButton("探索開始", on_click=self.button_clicked),
         ],alignment=ft.MainAxisAlignment.START,
         ),
         ]
-        super().__init__("/", controls=controls)
+        
+        super().__init__("/", controls=controls, scroll=ft.ScrollMode.ALWAYS)
         self.start_URL = start_URL
         self.target_domains = target_domains
         
@@ -48,66 +47,80 @@ class Top(ft.View):
         self.update()
 
     def button_clicked(self,e):
+        URL = self.start_URL.current.value
+        #stayt_URLが空なら何もしない
+        if URL == "" or Scraping.check_valid_URL(URL) == False:
+            return
         domains = []
-        for target_domain in self.target_domains:
+        for target_domain in self.target_domains:#指定したドメインを一つの配列にまとめる
             if target_domain.current.value in domains:
                 continue
             domains.append(target_domain.current.value)
             
-        self.data = [self.start_URL.current.value, domains]
-        e.page.go("/view1")
+        self.data = [URL, domains]
+        e.page.go("/view1")#ページ遷移
     
     def changed(self, e):
         self.data = e.control.value
         self.update()
 
 class View1(ft.View):
-    def __init__(self, pages:dict):
+    def __init__(self, pages:Page):
         page_count = int(1)#ページ数
         controls = [ft.AppBar(title=ft.Text("Top Page"), bgcolor=ft.colors.SURFACE_VARIANT),]      
-        super().__init__("/view1", controls=controls)
+        super().__init__("/view1", controls=controls, scroll=ft.ScrollMode.ALWAYS)
         for view_page in pages:#全てのデータを出力するための２重ループ
-            controls.append(ft.Text(f'{page_count}回目'))#何ページかを表示)
+            controls.append(ft.Text(f'{page_count}ページ目', size=18))#何ページかを表示)
+            
+            controls.append(ft.Text(f'元URL : {view_page.my_URL}'))
             
             for URL in view_page.URL:#URLの表示
-                controls.append(ft.Text(f'URL:{URL}'))
+                controls.append(ft.Text(f'URL : {URL}', selectable=True))
                 
             for title in view_page.title:#タイトルの表示
-                controls.append(ft.Text(f'title:{title}'))
+                controls.append(ft.Text(f'title : {title}'))
 
             for keyword in view_page.keyword:#キーワードの表示                
-                controls.append(ft.Text(f'keyword:{keyword}'))
-
-            for parameters in view_page.parameters:#パラメータの表示
-                controls.append(ft.Text(f'{parameters}'))
-
+                controls.append(ft.Text(f'keyword : {keyword}'))
+            
+            controls.append(ft.Text("parameters : {"))#パラメータの表示
+            for key in view_page.parameters:
+               controls.append(ft.Text(f"    {key} : {view_page.parameters[key]}"))
+            controls.append(ft.Text("}"))
+            
+            controls.append(ft.Text(f'',size=10))
             page_count += 1#カウントアップ
+
+        #最後にキーワード一覧出力
+        controls.append(ft.Text('[keyword]'))
+        for pages in pages:
+            if pages.keyword is True:
+                controls.append(ft.Text(f'{pages.keyword}, '))               
+
+            
+    
         
 def main(page: ft.Page):
     page.title         = "ごとうぐみ"
     page.window_width  = 650
     page.window_height = 1000
-    pop_flag = False
 
     def route_change(route):
-        nonlocal pop_flag
-
-        if pop_flag:
-            pop_flag = False
-        else:
-            if page.route == "/":
-                print('top')
-                page.views.clear()
-                page.update()
-                page.views.append(
-                    Top()
-                )
-            elif page.route == "/view1":
-                print('view1')
-                pages = Crawler.crawler(start_URL = page.views[-1].data[0], target_domins=page.views[-1].data[1])
-                page.views.append(
-                    View1(pages)
-                )
+        if page.route == "/":
+            print('topですよ')
+            page.views.clear()
+            page.update()
+            page.views.append(
+                Top()
+            )
+        elif page.route == "/view1":
+            if page.views[-1].data[1] == [""]:
+                page.views[-1].data[1] = []
+            pages = Crawler.crawler(start_URL = page.views[-1].data[0], target_domins=page.views[-1].data[1])
+            page.views.append(
+                View1(pages)
+            )
+            print('view1ですよ')
         
     def view_pop(e):
         page.views.pop()
